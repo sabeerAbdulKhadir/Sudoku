eaad899a5750e96f1ec21eb82b8046778e1df567
sabeer <sabeer@Sabzz>
2016-11-12 18:49:28 +1100

    removed point

--- src/com/sudoku/Cell.java
+++ src/com/sudoku/Cell.java
@@ -2,22 +2,10 @@ package com.sudoku;
 
 public class Cell {
 
-	private Point point;
-
 	private String value;
 
 	public Cell(String value, Point point) {
 		this.value = value;
-		this.point = point;
-	}
-
-	public Point getPoint() {
-		return point;
-	}
-
-	public void setPoint(Point point) {
-		this.point = point;
-
 	}
 
 	public String getValue() {
